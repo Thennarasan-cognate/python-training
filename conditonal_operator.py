d9191ffbd7f3b90962a1a3ec98e94495497eb658
Thennarasan-cognate <85215492+Thennarasan-cognate@users.noreply.github.com>
2021-06-18 11:53:15 +0530

    Add files via upload

---
+++ conditonal_operator.py
@@ -0,0 +1,12 @@
+a=input('Enter the value of a:')
+b=input('Enter the value of b:')
+
+if a>b:
+    print('a is greater than b')
+
+elif b>a:
+    print('b is greater than a')
+
+else:
+    print('a is equal to b')
+    
