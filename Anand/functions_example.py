267e7cffaa4b36be5f69acaa9f2e2aa19d1f9b4d
[anandkeerthipati219] <[anand.k@cognateglobal.com]>
2021-06-24 11:48:15 +0530

    Create functions_example.py

---
+++ Anand/functions_example.py
@@ -0,0 +1,22 @@
+'''def earpodes(brand,price,rating):
+    print('the brand is {} and price is {} and the rating is {}'.format(brand,price,rating))
+earpodes('jbl',5000,4.0)'''
+
+
+'''def hello(reg_name):            
+	print("memory location after call within function ",id(reg_name))
+	print(reg_name)
+	return
+student="vivek"
+print("memory location before call i,e before passing variable to function  ",id(student))
+hello(student) '''
+
+
+a='anand'
+def a():
+    percentage=66.6
+    print("percentage is :",percentage)
+    return
+a()
+print("name outside the function:",a)
+    
