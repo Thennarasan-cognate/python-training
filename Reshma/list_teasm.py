3a5045194498779574e62d4e17ceb4373ef2081e
priya111260 <85218963+priya111260@users.noreply.github.com>
2021-06-24 11:54:42 +0530

    Add files via upload

---
+++ Reshma/list_teasm.py
@@ -0,0 +1,15 @@
+for x in ['trichy','35','2','2+4j','25.5']:                  
+    if x=='2':
+        print("i am going place")
+        break
+    print(x)
+    print('hello')
+    print("done")
+
+for x in ['trichy','35','2','2+4j','25.5']:                  
+    if x=='2':
+        print("i am going place")
+        continue
+    print(x)
+    print('hello')
+    print("done")
